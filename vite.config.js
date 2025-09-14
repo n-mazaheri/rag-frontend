45b0986e2cc23960b8c4c5b39793abb21015dac6
Nasrin <na.mazaheri@yahoo.com>
2025-09-15 02:18:40 +0330

    revert changes

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [react(),tailwindcss(),]
 })
